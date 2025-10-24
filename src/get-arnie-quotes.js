fbca8fc8c341600ac35814bcaf077dcce91cc887
henrl <henrylau111@yahoo.com.au>
2025-10-24 13:05:52 +1100

    Build getArnieQuotes function - "Allow me to break the ice"

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,25 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  return await Promise.all(
+    urls.map(async (url) => {
+      return await getQuote(url);
+    })
+  );
 };
+
+const getQuote = async (url) => {
+  const res = await httpGet(url);
+  const payload = JSON.parse(res.body).message;
+  if (res.status === 200) {
+    return {
+      'Arnie Quote': payload
+    };
+  }
+  return {
+    'FAILURE': payload
+  };
+}
 
 module.exports = {
   getArnieQuotes,
